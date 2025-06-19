fde57e1eadb715f43f1a4f2b56b4b7ea0c7c012b
Danu Wahyurinata <dhanu.wahyurinata16@gmail.com>
2025-06-19 23:12:25 +0700

    Update

---
+++ GUI/UAS RILL/Models/Tugas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TugasManager.Models
+{
+    public class Tugas
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Judul { get; set; }
+        public string Deskripsi { get; set; }
+        public DateTime TanggalDeadline { get; set; }
+    }
+}
---
+++ GUI/UAS RILL/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TugasManager.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Nama { get; set; }
+        public string Role { get; set; } // "Guru" atau "Siswa"
+    }
+}
